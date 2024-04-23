1793d0d07a4e9326e6d0587022e938aa31a9353f
Davey Mason <daveymason@outlook.com>
2024-04-23 20:42:34 +0200

    new

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
     return (
       <Grid container spacing={2} sx={{bgcolor: '#00AEEF', mt:5 }}>
       <footer style={{backgroundColor: '#00AEEF', color: 'white', textAlign: 'center', padding: '3px', position: 'fixed', left: '0', bottom: '0', width: '100%'}}>
-        <p>© {new Date().getFullYear()} Invoice Integrator. All Rights Reserved.</p>
+        <p>© {new Date().getFullYear()} Invoice Integrator. </p>
       </footer>
     </Grid>
     );
